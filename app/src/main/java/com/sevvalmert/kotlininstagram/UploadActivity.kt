d755f70cc6f58614b69220394afc846fd6bdbafe
sserifemert <sserifemert@gmail.com>
2025-03-12 23:11:10 +0300

    added firestore collection

--- app/src/main/java/com/sevvalmert/kotlininstagram/UploadActivity.kt
+++ app/src/main/java/com/sevvalmert/kotlininstagram/UploadActivity.kt
@@ -3,9 +3,12 @@ package com.sevvalmert.kotlininstagram
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Base64
 import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -17,7 +20,17 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.storage
 import com.sevvalmert.kotlininstagram.databinding.ActivityUploadBinding
+import java.io.ByteArrayOutputStream
+import java.util.UUID
 
 class UploadActivity : AppCompatActivity() {
 
@@ -25,6 +38,9 @@ class UploadActivity : AppCompatActivity() {
     private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher : ActivityResultLauncher<String>
     var selectedPicture : Uri? = null
+    private lateinit var  auth : FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var storage : FirebaseStorage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +54,40 @@ class UploadActivity : AppCompatActivity() {
             insets
         }
 
+        auth = Firebase.auth
+        firestore = Firebase.firestore
+        storage = Firebase.storage
+
         registerLauncher()
+    }
+
+    fun convertImageToBase64(uri: Uri): String {
+        val inputStream = contentResolver.openInputStream(uri)
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+        val outputStream = ByteArrayOutputStream()
+
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
+        val byteArray = outputStream.toByteArray()
+
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
     }
 
     fun uploadClicked(view : View){
 
+        if (selectedPicture != null) {
+            val base64Image = convertImageToBase64(selectedPicture!!)
+            val postMap = hashMapOf<String, Any>()
+            postMap.put("image", base64Image)
+            postMap.put("userEmail", auth.currentUser!!.email!!)
+            postMap.put("comment", binding.commentText.text.toString())
+            postMap.put("date", Timestamp.now())
+
+            firestore.collection("Posts").add(postMap).addOnSuccessListener {
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     fun selectImageClicked(view : View){
